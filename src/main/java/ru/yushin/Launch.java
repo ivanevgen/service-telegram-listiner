a582c0665a698c4e8347ec93ec9bcc23d55a624b
ivanevgen <tasher19906@gmail.com>
2021-07-24 00:20:05 +0300

    delete comment

--- src/main/java/ru/yushin/Launch.java
+++ src/main/java/ru/yushin/Launch.java
@@ -11,10 +11,6 @@ public class Launch {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             Bot bot = new Bot();
-
-
-            //todo Как конвертировать мою программу Java в файл .exe?
-            //todo https://coderoad.ru/147181/%D0%9A%D0%B0%D0%BA-%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%BC%D0%BE%D1%8E-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%83-Java-%D0%B2-%D1%84%D0%B0%D0%B9%D0%BB-exe
             BotSession botSession = botsApi.registerBot(bot);
         } catch (TelegramApiException e) {
             e.printStackTrace();
